716927c1060c59c6ce6b33450e61418372ea0a3c
mateushedp <mateushedlundp@gmail.com>
2026-03-24 16:17:51 -0300

    fix: fix icon alignment in TechBadge with fixed height

--- src/components/tech-badge.tsx
+++ src/components/tech-badge.tsx
@@ -7,7 +7,8 @@ type ComponentProps = {
 function TechBadge({ title, iconSrc  }: ComponentProps) {
 	return (
 		<div className="flex flex-col justify-center w-[100px] h-[120px] items-center gap-3 p-3 bg-mainGreen border border-black rounded-lg drop-shadow-brutalist-md text-beige font-unigeo text-sm">
-			<img src={iconSrc} alt={`${title} icon`} className="w-[48px] h-auto" />
+			<img src={iconSrc} alt={`${title} icon`} className="w-[48px] h-[48px] object-contain" />
+
 			<p className="font-unigeo text-base">{title}</p>
 		</div>
 	);
